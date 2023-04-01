d4e00c3a4e86149889070adfc17b92ccc7d95943
Bjarne <92723881+bjarnerossen@users.noreply.github.com>
2023-04-02 10:54:40 +1200

    improved food-emoji float and glow on index.html. Also improved the about.html site. Removed some js animations and added spotify lyrics effect, plus floating guru.

--- Project/js/about.js
+++ Project/js/about.js
@@ -11,35 +11,70 @@ function addIndexToOL() {
   
 addIndexToOL();
 
-window.addEventListener('scroll', function() {
-    var heroHeight = document.querySelector('.hero').offsetHeight;
-    var scrollTop = document.documentElement.scrollTop;
-    var divider = document.querySelector('.divider');
-    
-    if (scrollTop > heroHeight) {
-      divider.style.display = 'block';
+function handleScroll() {
+  const howItWorksSection = document.querySelector('.content h2:nth-of-type(2)');
+  const listItems = document.querySelectorAll('.content ol li');
+  const ol = document.querySelector('.content ol');
+  const viewportHeight = window.innerHeight;
+
+  if (window.scrollY >= howItWorksSection.offsetTop - 200) {
+    document.body.style.backgroundColor = 'var(--accent-color-2)';
+    document.querySelector('.content').style.color = 'black';
+  } else {
+    document.body.style.backgroundColor = '';
+    document.querySelector('.content').style.color = 'white';
+    ol.style.backgroundColor = '';
+    ol.style.borderRadius = '';
+    ol.style.padding = '';
+    listItems.forEach((item) => {
+      item.style.color = '';
+      item.style.fontSize = '';
+      item.style.transition = '';
+    });
+  }
+
+  listItems.forEach((item, index) => {
+    const itemHeight = item.offsetHeight;
+    const itemOffsetTop = item.offsetTop;
+    const itemMiddle = itemOffsetTop + itemHeight / 2 - viewportHeight / 2;
+
+    if (window.scrollY >= itemMiddle) {
+      const nextItem = listItems[index + 1];
+      const prevItem = listItems[index - 1];
+
+      item.style.color = 'white';
+      item.style.fontSize = '24px';
+      item.style.transition = 'color 0.5s ease-in-out, font-size 0.5s 0.2s ease-in-out';
+      
+      if (nextItem) {
+        nextItem.style.color = 'rgba(0,0,0,0.5)';
+        nextItem.style.fontSize = '16px';
+        nextItem.style.transition = 'color 0.5s 0.2s ease-in-out, font-size 0.5s 0.4s ease-in-out';
+      }
+      
+      if (prevItem) {
+        prevItem.style.color = 'rgba(0,0,0,0.5)';
+        prevItem.style.fontSize = '16px';
+        prevItem.style.transition = 'color 0.5s 0.2s ease-in-out, font-size 0.5s 0.4s ease-in-out';
+      }
     } else {
-      divider.style.display = 'none';
+      item.style.color = '';
+      item.style.fontSize = '';
+      item.style.transition = '';
     }
   });
 
-  
-  const content = document.querySelector('.content');
-
-  function handleScroll() {
-    const hero = document.querySelector('.hero');
-    const divider = document.querySelector('.divider');
-  
-    if (window.pageYOffset > hero.offsetHeight) {
-      content.style.marginLeft = '18rem';
-      content.style.transition = 'margin-left 0.3s ease-in-out';
-      divider.style.display = 'block';
-    } else {
-      content.style.marginLeft = '0';
-      content.style.transition = 'margin-left 0.3s ease-in-out';
-      divider.style.display = 'none';
-    }
+  if (window.scrollY >= listItems[0].offsetTop - viewportHeight * 0.8) {
+    ol.style.backgroundColor = 'darkslateblue';
+    ol.style.borderRadius = '10px';
+    ol.style.padding = '20px';
+    ol.style.transition = 'background-color 0.5s ease-in-out';
+  } else {
+    ol.style.backgroundColor = '';
+    ol.style.borderRadius = '';
+    ol.style.padding = '';
+    ol.style.transition = 'background-color 0.5s ease-in-out';
   }
-  
-  window.addEventListener('scroll', handleScroll);
-  
+}
+
+window.addEventListener('scroll', handleScroll);
